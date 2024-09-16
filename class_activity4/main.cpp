5ed345ebc9bceac8456cc7b681e149909617226d
Sonia <30045332@cc.peralta.edu>
2024-09-15 21:51:17 -0700

    Initial Commit

---
+++ class_activity4/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  class_activity4
+//
+//  Created by Sonia Lopchan on 9/15/24.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+    cout << setw(10)<<left<<"COURSE" << setw(10) << right << "STUDENT" << endl;
+    cout << setw(10)<<left<<"C++" << setw(10) << right << 100 << endl;
+    cout << setw(10)<<left<<"JavaScript" << setw(10) << right << 50 << endl;
+    
+    return 0;
+}
